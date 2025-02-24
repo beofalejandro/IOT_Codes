846bc36e6d5922f0a08a9c8ca7c03d7de02753e4
Alejandro Barrientos <112975406+beofalejandro@users.noreply.github.com>
2025-02-24 12:35:33 -0600

    0.5 stable - Fix some issues

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ from gpiozero import LED, Buzzer
 import time
 from time import strftime, sleep
 from DFRobot_DHT20 import DFRobot_DHT20 as DFRobot
-
-import random as rdm
 import threading as threating
 
-app = fk(__name__)
+app = fk(_name_)
 socketio = SocketIO(app)
 
 I2C_BUS = 0x01  # default use I2C1 bus
@@ -16,43 +14,33 @@ I2C_ADDRESS = 0x38  # default I2C device address
 
 dht20 = DFRobot(I2C_BUS, I2C_ADDRESS)
 
-
 @app.route('/')
 def index():
-
     return rt('index.html')
-
-
-def generate_data():
-    while True:
-        if not dht20.begin():
-            print("DHT20 sensor initialization failed")
-        else:
-            while True:
-                print(strftime("%Y-%m-%d %H:%M:%S %Z"))
-                T_celcius, humidity, crc_error = dht20.get_temperature_and_humidity()
-        if crc_error:
-            print("CRC               : Error\n")
-        else:
-            T_fahrenheit = T_celcius*9/5 + 32
-            print("Temperature       : %f\u00b0C / %f\u00b0F" %(T_celcius, T_fahrenheit))
-            print("Relative Humidity : %f %%" %humidity)
-            print("CRC               : OK\n")
-            sleep(5)  
-        tdata = rdm.randint(1, 100)
-        hdata = rdm.randint(1, 1000)
-        socketio.emit('t_new_data', {'value':T_celcius})
-        socketio.emit('h_new_data', {'value':humidity})
-        time.sleep(5)
 
 @app.route('/iraprueba')
 def prueba():
     return rt('prueba.html')
 
-if __name__ == '__main__':
-    thread = threating.Thread(target=generate_data)
-    thread.start()
-    socketio.run(app)
+def generate_data():
+    while True:
+        if not dht20.begin():
+            print("DHT20 sensor initialization failed")
+            socketio.emit('sensor_error', {'message': "DHT20 sensor initialization failed"})
+            sleep(5)
+        else:
+            T_celcius, humidity, crc_error = dht20.get_temperature_and_humidity()
+            if crc_error:
+                print("CRC Error")
+                socketio.emit('sensor_error', {'message': "CRC Error"})
+            else:
+                T_fahrenheit = T_celcius * 9 / 5 + 32
+                socketio.emit('t_new_data', {'value': T_celcius})
+                socketio.emit('h_new_data', {'value': humidity})
+            sleep(5)
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5001, debug=True)
+if _name_ == '_main_':
+    thread = threating.Thread(target=generate_data)
+    thread.daemon = True
+    thread.start()
+    socketio.run(app, host="0.0.0.0", port=5001, debug=True, allow_unsafe_werkzeug=True)
